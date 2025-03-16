a08978116880ba0dad305937b0968a3c8999d7a8
luisparrax <114308440+luisparrax@users.noreply.github.com>
2025-03-16 11:41:19 +0100

    actualizando puerto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000; // Usa el puerto que Render asigna
+const PORT = process.env.PORT || 10000; // Usa el puerto que Render asigna
 
 app.use(cors());
 app.use(bodyParser.json());
